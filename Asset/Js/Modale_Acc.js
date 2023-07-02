2c7f73bc0f107df32e57558c15d88dfafdebcf35
Crtmetton <crtmetton@gmail.com>
2023-07-02 23:50:13 +0200

    Init_Branch

--- Asset/Js/Modale_Acc.js
+++ Asset/Js/Modale_Acc.js
@@ -1,4 +1,4 @@
-document.getElementById("btn-Acc").addEventListener("click", ()=>{
+document.getElementById("Acc-connect").addEventListener("click", ()=>{
     // document.getElementById("Acc").classList.remove("d-none")
     location = "../../account/connect.php"
     window.location
--- index.php
+++ index.php
@@ -8,75 +8,93 @@ include("./includer/db-connect.php");
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" href="Asset/Css/Main.css">
+    <link rel="stylesheet" href="Asset/Css/main.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tiny Plant</title>
 </head>
-<button id="btn-Acc">A delete</button>
+
 <body>
+ <header>    <div class="top-band">
+         <div class="right-top-bar">
+             <div class="logo">
+                 <img id="logo" src="Asset/Svg/Logo_Icon.svg">
+             </div>
+             <div class="left-side">
+                 <img id="Acc-connect" class="Icon-top-bar" src="Asset/Svg/user-thin.svg">
+                 <img class="Icon-top-bar" src="Asset/Svg/heart-thin.svg">
+                 <img class="Icon-top-bar" src="Asset/Svg/bag-shopping-thin.svg">
+             </div>
+         </div>
+     </div>
+ </header>
+ <section id="Mid">
+     <div class="real-top">
+     <?php
+     $categories = $bdd->query('SELECT * FROM Categories');
+     while ($category = $categories->fetch()){
+         echo '<a href="./category.php?category='. $category['scope'] .'">'.$category['name'].'</a>';
+     }
+     ?>
+     </div>
         <div class="window-product">
-            <div id="Firt"></div>
             <div class="descriptif">
                 <div class="titre-produit">Titre produit</div>
             </div>
         </div>
-
         <div class="hug-content">
             <div class="content-frame">
-                <div class="prod-1">
-                    <img class="rectangle-1" src="rectangle-1.png" />
-
+                <div class="prod">
+                    <img class="Product_Img" src="Asset/Product-files/prod_img1.jpg"/>
                     <div class="id-produit">Id Produit</div>
                 </div>
 
-                <div class="prod-2">
-                    <img class="rectangle-12" src="rectangle-12.png" />
+                <div class="prod">
+                    <img class="Product_Img" src="Asset/Product-files/prod_img2.jpg" />
 
                     <div class="id-produit2">Id Produit</div>
                 </div>
 
-                <div class="prod-3">
-                    <img class="rectangle-13" src="rectangle-13.png" />
+                <div class="prod">
+                    <img class="Product_Img" src="Asset/Product-files/prod_img3.jpg" />
 
                     <div class="id-produit3">Id Produit</div>
                 </div>
             </div>
 
-            <div class="content-frame2">
-                <div class="prod-1">
-                    <img class="rectangle-14" src="rectangle-14.png" />
+            <div class="content-frame">
+                <div class="prod">
+                    <img class="Product_Img" src="Asset/Product-files/prod_img4.jpg" />
 
                     <div class="id-produit4">Id Produit</div>
                 </div>
 
-                <div class="prod-2">
-                    <img class="rectangle-15" src="rectangle-15.png" />
+                <div class="prod">
+                    <img class="Product_Img" src="Asset/Product-files/prod_img1.jpg" />
 
                     <div class="id-produit5">Id Produit</div>
                 </div>
 
-                <div class="prod-3">
-                    <img class="rectangle-16" src="rectangle-16.png" />
+                <div class="prod">
+                    <img class="Product_Img" src="Asset/Product-files/prod_img3.jpg" />
 
                     <div class="id-produit6">Id Produit</div>
                 </div>
             </div>
-
-            <div class="content-frame3">
-                <div class="prod-1">
-                    <img class="rectangle-17" src="rectangle-17.png" />
+            <div class="content-frame">
+                <div class="prod">
+                    <img class="Product_Img" src="Asset/Product-files/prod_img2.jpg" />
 
                     <div class="id-produit7">Id Produit</div>
                 </div>
 
-                <div class="prod-2">
-                    <img class="rectangle-18" src="rectangle-18.png" />
+                <div class="prod">
+                    <img class="Product_Img" src="Asset/Product-files/prod_img4.jpg" />
 
                     <div class="id-produit8">Id Produit</div>
                 </div>
 
-                <div class="prod-3">
-                    <img class="rectangle-19" src="rectangle-19.png" />
+                <div class="prod">
+                    <img class="Product_Img" src="rAsset/Product-files/prod_img2.jpg" />
 
                     <div class="id-produit9">Id Produit</div>
                 </div>
@@ -88,15 +106,11 @@ include("./includer/db-connect.php");
                 <div class="titre-produit">Titre produit</div>
             </div>
         </div>
+    <button id="btn-Acc">A delete</button>
+ </section>
+<footer>
 
-        <div class="real-top">
-            <?php
-            $categories = $bdd->query('SELECT * FROM Categories');
-            while ($category = $categories->fetch()){
-                echo '<a href="./category.php?category='. $category['scope'] .'">'.$category['name'].'</a>';
-            }
-            ?>
-        </div>
+</footer>
 </body>
 <script src="../Asset/Js/Modale_Acc.js"></script>
 </html>
